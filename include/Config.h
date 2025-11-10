b9713409a6ab3e73757fa168aa2babc089b44f54
garypenman <gary.penman@glasgow.ac.uk>
2025-11-10 17:57:24 +0000

    Added the start of a database / config header setup to set global values needed for the different analysis inputs

---
+++ include/Config.h
@@ -0,0 +1,23 @@
+const int epic_beam_ele_idx=0; 
+const int epic_beam_ion_idx=3; 
+const int epic_scat_ele_idx=1; 
+const int epic_scat_ion_idx=5; 
+const int epic_lep_plus_idx=6; 
+const int epic_lep_minus_idx=7;
+
+const int eslight_beam_ele_idx=0; 
+const int eslight_beam_ion_idx=3; 
+const int eslight_scat_ele_idx=2; 
+const int eslight_scat_ion_idx=6; 
+const int eslight_lep_plus_idx=5; 
+const int eslight_lep_minus_idx=4;
+
+const int elspectro_beam_ele_idx=0; 
+const int elspectro_beam_ion_idx=1; 
+const int elspectro_scat_ele_idx=2; 
+const int elspectro_scat_ion_idx=3; 
+const int elspectro_lep_plus_idx=4; 
+const int elspectro_lep_minus_idx=5;
+
+const std::string farm_dir = "/w/work5/home/garyp/eic/Farm/";
+const std::string out_dir = "/w/work5/home/garyp/rad_trees/";
