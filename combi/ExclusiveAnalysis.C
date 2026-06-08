2f8d50d643557db41b8339a787955e4b2956bd8f
garypenman <gary.penman@glasgow.ac.uk>
2026-06-08 15:56:46 +0100

    update exclusive analysis to use 10x130 recon, labelled MissM2 plot

--- combi/ExclusiveAnalysis.C
+++ combi/ExclusiveAnalysis.C
@@ -1,11 +1,16 @@
 #include "../include/Config.h"
-#include "/home/gp140f//epic_plot_macro/ePIC_style.C"
+#include "..//epic_plot_macro/ePIC_style.C"
 
 std::string plus_dir = "/w/work6/home/gp140f/combirad_trees/Campaign26.03.1/ddvcs_ee_18x275_hplus/";
 std::string minus_dir = "/w/work6/home/gp140f/combirad_trees/Campaign26.03.1/ddvcs_ee_18x275_hminus/";
 
-void ExclusiveAnalysis(std::string infiledir = plus_dir, std::string beam_setting = "18x275"){
+std::string bg_dir = "/w/work6/home/gp140f/tcs_bg_merge/bg_rad/";
+std::string nobg_dir = "/w/work6/home/gp140f/tcs_bg_merge/nobg_rad/";
 
+void ExclusiveAnalysis(std::string infiledir = bg_dir, std::string beam_setting = "18x275"){
+
+  set_ePIC_style();
+  
   // ----------------------------------------------------------
   // Output
   // ----------------------------------------------------------
@@ -20,13 +25,16 @@ void ExclusiveAnalysis(std::string infiledir = plus_dir, std::string beam_settin
   ROOT::RDataFrame df_rec_base("tree", rec_filename);
   auto df_rec_tru = df_rec_base.Filter("isTruth==1");
   
-  auto hrec_MissMass2 = df_rec_base.Histo1D({"hrec_MissMass2","",250,-15,15},"rec_MissMass2");
-  auto hrec_MissMass2_tru = df_rec_tru.Histo1D({"hrec_MissMass2_tru","",250,-15,15},"rec_MissMass2");
+  auto hrec_MissMass2 = df_rec_base.Histo1D({"hrec_MissMass2","All Brem BG Combis; M^{2}_{miss} (e' e^{+}e^{-} p') [GeV/c]; Events / 0.12 GeV/c",250,-15,15},"rec_MissMass2");
+  //auto hrec_MissMass2_cut = df_rec_base.Filter().Histo1D({"hrec_MissMass2","All Brem BG Combis",250,-15,15},"rec_MissMass2");
+  auto hrec_MissMass2_tru = df_rec_tru.Histo1D({"hrec_MissMass2_tru","Truth Events",250,-15,15},"rec_MissMass2");
+
   
   //hrec_MissMass2->Fit("gaus","");
+  hrec_MissMass2->SetMinimum(0);
   hrec_MissMass2->DrawCopy();
   hrec_MissMass2_tru->SetLineColor(kRed);
   hrec_MissMass2_tru->DrawCopy("same");
   //gPad->SetLogy();
-  
+  gPad->BuildLegend();
 }
